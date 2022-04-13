0be5df38b701bcc788422d3566fd0f8566848247
Jhonathan Vitor <jhonathanvitor@gmail.com>
2022-04-13 17:43:19 -0400

    Criated Connerction Strings

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Mvc;
 using ProEventos.API.Models;
+using System;
+using System.Collections.Generic;
 
 namespace ProEventos.API.Controllers
 {
-  [ApiController]
-  [Route("[api/controller]")]
+    [ApiController]
+  [Route("api/controller")]
   public class EventoController : ControllerBase
   {
     public IEnumerable<Evento> _evento = new Evento[] {
